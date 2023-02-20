6d23d6177d297010dc3846c175352059b68a9396
Dikshit singh <singhdikshit69@gmail.com>
2023-02-20 13:22:04 +0530

    modified g.sh #2

--- g.sh
+++ g.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-echo "Hi"
+
+#status before tracking/staged zone
 git status
+
+#start tracking files
 git add .
+
+#status after started tracking the files
 git status
+
 #Read the user input 
 echo "Enter your commit message"
 read commit_message
-echo "You entered $commit_message"
+
+#Commiting the changes
 git commit -m "$commit_message"
+
+#status after commiting the files
 git status
+
+#message that commit successfully
 echo "Commit successfully"
+
+#push automatically the changes into your remote repository
 git push
+
+
+#checking status after all again
 git status
