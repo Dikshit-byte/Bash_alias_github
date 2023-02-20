08794c6cb8a88a4d929db188193ad07855bbf01c
Dikshit singh <singhdikshit69@gmail.com>
2023-02-20 13:15:55 +0530

    modified g.sh

--- g.sh
+++ g.sh
@@ -2,7 +2,13 @@
 echo "Hi"
 git status
 git add .
+git status
 #Read the user input 
 echo "Enter your commit message"
 read commit_message
 echo "You entered $commit_message"
+git commit -m "$commit_message"
+git status
+echo "Commit successfully"
+git push
+git status
